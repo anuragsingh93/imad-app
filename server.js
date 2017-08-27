9beb07307877a087719334e6e5683cb798cec61b
anuragsingh93 <anuragsinghcs0027@gmail.com>
2017-08-27 15:31:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,13 +4,99 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articles= {
+    'article-one': {
+        title : 'Article One | Anurag Singh',
+        heading : 'Article One',
+        date : 'Aug 27 , 2017',
+        content : `
+                    <p>
+                        This is the content of my first article
+                    </p>
+                    <p>
+                        This is the content of my first article
+                    </p>
+                    <p>
+                        This is the content of my first article
+                    </p>`
+    },
+    'article-two': {
+        title : 'Article Two | Anurag Singh',
+        heading : 'Article Two',
+        date : 'Aug 27 , 2017',
+        content : `
+                    <p>
+                        This is the content of my second article
+                    </p>
+                    <p>
+                        This is the content of my second article
+                    </p>
+                    <p>
+                        This is the content of my second article
+                    </p>`
+    },
+    'article-three': {
+        title : 'Article Three | Anurag Singh',
+        heading : 'Article Three',
+        date : 'Aug 27 , 2017',
+        content : `
+                    <p>
+                        This is the content of my third article
+                    </p>
+                    <p>
+                        This is the content of my third article
+                    </p>
+                    <p>
+                        This is the content of my third article
+                    </p>`
+    }
+    
+};
 
+function createTemplate(data){
+    var title= data.title;
+    var heading= data.heading;
+    var date =data.date;
+    var content= data.content;
+    var htmlTemplate =`
+    <html>
+        <head>
+            <title>
+               ${title}
+            </title>  
+            <meta name="viewport" content="width=device-width, initial=svcale=1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>       
+        <body>
+            <div class="container">
+                <div>
+                <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>    
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>    
+    </html>
+    
+    `;
+    return htmlTemplate;
+
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req, res) {
+    articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 
