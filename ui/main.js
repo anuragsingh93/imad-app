6a870e19a074f6e25ae775e62d4e4096a4c05b0a
anuragsingh93 <anuragsinghcs0027@gmail.com>
2017-08-27 20:36:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,18 @@ button.onclick = function () {
     request.open('GET', 'http://anuragsinghcs0027.imad.hasura-app.io/counter',true);
     request.send(null);
 };
+
+
+var nameInput = document.getElementById('name');
+var nam = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    
+    var name=['name1' , 'name2' , 'name3'];
+    var list='';
+    for(var i=0 ; i< names.length; i++){
+        list += '<li>' +name[i]+ '</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML = list;
+};
