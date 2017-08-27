df42e786335b7d4d981ad5a35fdf15fb4bdadfc8
anuragsingh93 <anuragsinghcs0027@gmail.com>
2017-08-27 15:48:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
-element.innerHtml='New Value'
+element.innerHtml='New Value';
+
+//move the image
+
+var img=document.getElementById('madi');
+madi.onClick =function(){
+  img.style.marginLeft='100px';
+};
